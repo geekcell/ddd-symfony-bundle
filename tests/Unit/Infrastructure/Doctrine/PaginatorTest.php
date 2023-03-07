2bbafd2cb9acf76b85a90dbbc71f6dac904f2068
janvt <janvt@me.com>
2023-03-07 13:46:31 +0100

    fix test class name

--- tests/Unit/Infrastructure/Doctrine/PaginatorTest.php
+++ tests/Unit/Infrastructure/Doctrine/PaginatorTest.php
@@ -9,7 +9,7 @@ use GeekCell\DddBundle\Infrastructure\Doctrine\Paginator as DoctrinePaginator;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
-class DoctrinePaginatorTest extends TestCase
+class PaginatorTest extends TestCase
 {
     /** @var OrmPaginator|Mockery\MockInterface */
     private mixed $ormPaginatorMock;
@@ -21,7 +21,7 @@ class DoctrinePaginatorTest extends TestCase
         $this->ormPaginatorMock = Mockery::mock(OrmPaginator::class);
     }
 
-    /**
+    /**g
      * @dataProvider provideCurrentPageData
      *
      * @param int $first
