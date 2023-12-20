3b2ca94559006b3cbe707253cbed53eeedef9b82
Bl00D4NGEL <dominik.peters@cura4you.de>
2023-12-20 14:48:18 +0100

    test: add regression test for exception handling

--- tests/Unit/Infrastructure/Messenger/QueryBusTest.php
+++ tests/Unit/Infrastructure/Messenger/QueryBusTest.php
@@ -5,22 +5,29 @@ declare(strict_types=1);
 namespace GeekCell\DddBundle\Tests\Unit\Infrastructure\Messenger;
 
 use GeekCell\Ddd\Contracts\Application\Query;
+use GeekCell\DddBundle\Infrastructure\Messenger\CommandBus;
 use GeekCell\DddBundle\Infrastructure\Messenger\QueryBus;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Messenger\Handler\HandlersLocator;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
 
-/**
- * Test fixture for QueryBus.
- */
 class TestQuery implements Query
 {
 }
 
-/**
- * Test fixture for CommandBus.
- */
+class ThrowingQueryHandler
+{
+    public function __construct(private readonly \Exception $exceptionToThrow)
+    {
+    }
+
+    public function __invoke(TestQuery $command): mixed
+    {
+        throw $this->exceptionToThrow;
+    }
+}
+
 class TestQueryHandler
 {
     public function __invoke(TestQuery $query): mixed
@@ -45,6 +52,23 @@ class QueryBusTest extends TestCase
 
         // Then
         $this->assertSame(TestQuery::class, $result);
+    }
+
+    public function testDispatchFailsAndRethrowsException(): void
+    {
+        // Given
+        $expectedException = new \Exception('Not good enough');
+        $bus = $this->createMessageBus(
+            TestQuery::class,
+            new ThrowingQueryHandler($expectedException)
+        );
+        $commandBus = new QueryBus($bus);
+
+        // Then
+        $this->expectExceptionObject($expectedException);
+
+        // When
+        $commandBus->dispatch(new TestQuery());
     }
 
     private function createMessageBus(
